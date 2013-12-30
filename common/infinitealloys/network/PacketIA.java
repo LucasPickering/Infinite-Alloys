966d1995a249223b172c5553d65e01c63559f2cb
Lucas <lucas@Dr-No.(none)>
2013-12-30 18:03:18 -0500

    Bunch of GUI work, wand buttons *might* work now. Still needs testing and other important things.

--- common/infinitealloys/network/PacketIA.java
+++
@@ -1,9 +0,0 @@
-package infinitealloys.network;
-
-import net.minecraft.world.World;
-import com.google.common.io.ByteArrayDataInput;
-
-public interface PacketIA {
-
-	public void execute(World world, ByteArrayDataInput data);
-}
