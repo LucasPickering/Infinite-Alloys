d4e7200e6b22eea417ee67625c8b2d7e893f1b30
Lucas Pickering <bob.a.red.dino@gmail.com>
2015-01-30 13:51:31 -0500

    Fixed the home leash for bosses (still broken for bats)

--- src/main/java/infinitealloys/entity/EntityIABoss.java
+++ src/main/java/infinitealloys/entity/EntityIABoss.java
@@ -32,18 +32,25 @@ public abstract class EntityIABoss extends EntityMob {
     setSize(2F, 10F);
     isImmuneToFire = true;
     experienceValue = 50;
-    setHomeArea((int) posX, (int) posY, (int) posZ, 15); // Fix the entity to a certain area
 
     // All the AI tasks needed for proper operation
     tasks.addTask(0, new EntityAISwimming(this));
-    tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
-    tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
-    tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-    tasks.addTask(7, new EntityAIWander(this, 1.0D));
-    tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+    tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1D, false));
+    tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class,.0D, true));
+    tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1D));
+    tasks.addTask(7, new EntityAIWander(this, 2D));
+    tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8F));
     tasks.addTask(8, new EntityAILookIdle(this));
     targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
     targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+  }
+
+  @Override
+  public void onUpdate() {
+    super.onUpdate();
+    if (!hasHome()) {
+      setHomeArea((int) posX, (int) posY, (int) posZ, 15); // Fix the entity to a certain area
+    }
   }
 
   @Override
