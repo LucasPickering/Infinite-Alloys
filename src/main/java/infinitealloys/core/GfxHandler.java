107fd2299747739bc98d850c5bfd35099d759f4a
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-08 13:00:51 -0500

    Removed useless stuff from GfxHandler

--- src/main/java/infinitealloys/core/GfxHandler.java
+++ src/main/java/infinitealloys/core/GfxHandler.java
@@ -3,11 +3,16 @@ package infinitealloys.core;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import infinitealloys.client.gui.GuiInternetWand;
 import infinitealloys.client.gui.GuiOverlay;
@@ -46,158 +51,6 @@ public final class GfxHandler implements IGuiHandler {
     return null;
   }
 
-  /*
-  @Override
-  public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
-    if (block == IABlocks.machine) {
-      if (metadata < temInstances.length) {
-        TileEntityRendererDispatcher.instance.renderTileEntityAt(temInstances[metadata],
-                                                                 -0.5D, -0.5D, -0.5D, 0);
-      }
-    } else if (block == IABlocks.ore) {
-      Tessellator tessellator = Tessellator.instance;
-      block.setBlockBoundsForItemRender();
-      GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(0F, -1F, 0F);
-      renderer.renderFaceYNeg(block, 0D, 0D, 0D, block.getIcon(0, 0));
-      tessellator.draw();
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(0F, 1F, 0F);
-      renderer.renderFaceYPos(block, 0D, 0D, 0D, block.getIcon(1, 0));
-      tessellator.draw();
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(1F, 0F, 0F);
-      renderer.renderFaceXPos(block, 0D, 0D, 0D, block.getIcon(2, 0));
-      tessellator.draw();
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(-1F, 0F, 0F);
-      renderer.renderFaceXNeg(block, 0D, 0D, 0D, block.getIcon(3, 0));
-      tessellator.draw();
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(0F, 0F, -1F);
-      renderer.renderFaceZNeg(block, 0D, 0D, 0D, block.getIcon(4, 0));
-      tessellator.draw();
-
-      tessellator.startDrawingQuads();
-      tessellator.setNormal(0F, 0F, 1F);
-      renderer.renderFaceZPos(block, 0D, 0D, 0D, block.getIcon(5, 0));
-      tessellator.draw();
-
-      // Draw ore pieces on top of stone backgroundIcon
-      if (metadata < Consts.METAL_COUNT) {
-        int mult = EnumMetal.values()[metadata].color;
-        GL11.glColor4f((mult >> 16 & 255) / 255F, (mult >> 8 & 255) / 255F, (mult & 255) / 255F,
-                       1F);
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0F, -1F, 0F);
-        renderer.renderFaceYNeg(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0F, 1F, 0F);
-        renderer.renderFaceYPos(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(1F, 0F, 0F);
-        renderer.renderFaceXPos(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(-1F, 0F, 0F);
-        renderer.renderFaceXNeg(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0F, 0F, -1F);
-        renderer.renderFaceZNeg(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-
-        tessellator.startDrawingQuads();
-        tessellator.setNormal(0F, 0F, 1F);
-        renderer.renderFaceZPos(block, 0D, 0D, 0D, IABlocks.oreForegroundIcon);
-        tessellator.draw();
-      }
-      GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-    }
-  }
-
-  @Override
-  public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
-                                  RenderBlocks renderer) {
-    boolean rendered = true;
-    if (block == IABlocks.ore) { // Used to colorize the ores
-      rendered = renderer.renderStandardBlock(block, x, y, z);
-      Tessellator tessellator = Tessellator.instance;
-      int brightness = block.getMixedBrightnessForBlock(world, x, y, z);
-      int color = EnumMetal.values()[world.getBlockMetadata(x, y, z)].color;
-      float red = (color >> 16 & 255) / 255F;
-      float green = (color >> 8 & 255) / 255F;
-      float blue = (color & 255) / 255F;
-
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y - 1, z, 0)) {
-        tessellator.setBrightness(renderer.renderMinY > 0D ? brightness : block
-            .getMixedBrightnessForBlock(world, x, y - 1, z));
-        tessellator.setColorOpaque_F(red * 0.5F, green * 0.5F, blue * 0.5F);
-        renderer.renderFaceYNeg(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y + 1, z, 1)) {
-        tessellator.setBrightness(renderer.renderMaxY < 1D ? brightness : block
-            .getMixedBrightnessForBlock(world, x, y + 1, z));
-        tessellator.setColorOpaque_F(red, green, blue);
-        renderer.renderFaceYPos(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x + 1, y, z, 2)) {
-        tessellator.setBrightness(renderer.renderMinZ > 0D ? brightness : block
-            .getMixedBrightnessForBlock(world, x + 1, y, z));
-        tessellator.setColorOpaque_F(red * 0.6F, green * 0.6F, blue * 0.6F);
-        renderer.renderFaceXPos(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x - 1, y, z, 3)) {
-        tessellator.setBrightness(renderer.renderMaxZ < 1D ? brightness : block
-            .getMixedBrightnessForBlock(world, x - 1, y, z));
-        tessellator.setColorOpaque_F(red * 0.6F, green * 0.6F, blue * 0.6F);
-        renderer.renderFaceXNeg(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y, z - 1, 4)) {
-        tessellator.setBrightness(renderer.renderMinX > 0D ? brightness : block
-            .getMixedBrightnessForBlock(world, x, y, z - 1));
-        tessellator.setColorOpaque_F(red * 0.8F, green * 0.8F, blue * 0.8F);
-        renderer.renderFaceZNeg(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-      if (renderer.renderAllFaces || block.shouldSideBeRendered(world, x, y, z + 1, 5)) {
-        tessellator.setBrightness(renderer.renderMaxX < 1D ? brightness : block
-            .getMixedBrightnessForBlock(world, x, y, z + 1));
-        tessellator.setColorOpaque_F(red * 0.8F, green * 0.8F, blue * 0.8F);
-        renderer.renderFaceZPos(block, x, y, z, IABlocks.oreForegroundIcon);
-        rendered = true;
-      }
-    }
-    return rendered;
-  }
-
-  @Override
-  public boolean shouldRender3DInInventory(int modelID) {
-    return true;
-  }
-
-  @Override
-  public int getRenderId() {
-    return renderID;
-  }
-
   @SubscribeEvent
   public void onRenderGameOverlay(RenderGameOverlayEvent event) {
     if (!event.isCancelable() && event.type == RenderGameOverlayEvent.ElementType.EXPERIENCE) {
@@ -211,19 +64,18 @@ public final class GfxHandler implements IGuiHandler {
   }
 
   @SideOnly(Side.CLIENT)
-  @SuppressWarnings("unchecked")
   @SubscribeEvent
   public void onRenderWorldLast(RenderWorldLastEvent event) {
-    for (Point3 block : xrayBlocks.keySet()) {
-      renderBlockOutline(block.x, block.y, block.z, xrayBlocks.get(block));
+    for (Map.Entry<BlockPos, Integer> entry : xrayBlocks.entrySet()) {
+      final BlockPos pos = entry.getKey();
+      renderBlockOutline(pos.getX(), pos.getY(), pos.getZ(), entry.getValue());
     }
   }
-  */
 
   /**
-   * Draw a red outline around the block at the specific coordinates. To be specific, the
-   * coordinates of a block are the ones given in the debug menu while standing ON TOP OF that
-   * block. Convenience method for {@link #renderOutlineBox}.
+   * Draw a red outline around the block at the specific coordinates. To be specific, the coordinates
+   * of a block are the ones given in the debug menu while standing ON TOP OF that block. Convenience
+   * method for {@link #renderOutlineBox}.
    *
    * @param blockX the x-coordinate of the block to be outlined
    * @param blockY the y-coordinate of the block to be outlined
